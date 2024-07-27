5d0bcdf7347279544373f0193c7c4fb282414e6a
techdecipher <waytopranav22@gmail.com>
2024-07-27 13:59:31 +0530

    added codedeploy agent

--- pre-install-script/before_install.sh
+++ pre-install-script/before_install.sh
@@ -8,4 +8,11 @@ yum install httpd -y
 systemctl start httpd -y
 systemctl enable httpd -y
 
+# Install CodeDeploy Agent
+yum install -y ruby
+cd /home/ec2-user
+wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
+service codedeploy-agent start
 
